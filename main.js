b228c115eea59f992751bc9101de008a470b2e4b
Paul Jensen <paulbjensen@gmail.com>
2013-02-07 00:36:31 +0000

    fix for a client-side library loading issue

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ ss.client.templateEngine.use(require('ss-hogan'));
 ss.client.define('main', {
   view: 'app.html',
   css:  ['libs/reset.css', 'app.styl'],
-  code: ['libs/jquery.min.js', 'app' ],
+  code: ['libs/jquery.min.js', 'libs/chipmunk/cp.min.js', 'app' ],
   tmpl: '*'
 });
 
