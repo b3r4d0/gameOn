c4d434336c436e02afc43b7047b58b75ee72fe19
Brad Henderson <leaveluck2heaven@gmail.com>
2013-02-22 12:08:13 -0500

    about to implement the soul search procedure

--- client/code/app/vs/Cosmos.js
+++ client/code/app/vs/Cosmos.js
@@ -1,6 +1,38 @@
 "use strict";
 
 //983 Bushwick ( Bushwicks Living Room )
+//Dean Kamen formula for success: 
+//Pick something you love and make it your life's work. 
+//Do what is important and not what is easy.
+//Seymour Cray, when designing his supercomputers would maximally simplify the architecture 
+//in order to be sure his designs see the market. 
+//Cleverly balancing implementation speed against perfection, 
+//Cray has outdistanced everyone in the supercomputing field.
+//Creativity and cold meticulousness are often at odds. 
+//They require a different type of mind. 
+//A biologist will notice that they are based on a different neurohormonal brain profile! 
+//Tim Berners-Lee is an excellent example of a brilliantly creative mind, 
+//which is still able to focus on a task at hand, efficiently execute the plan of action, and make things happen. 
+//Remember Cray? He would give up bells and whistles only to be the first with a workable solution. 
+//If the expected payoff of the new goal is greater than that of the former goal, switch it. 
+//Otherwise persist even though your emotions may tell you that the new thing seems so much bigger 
+
+//Important Shit that Needs to Get Done
+
+//Achieve Funding for Kickstarter Game 	(July 	4)
+//Launch Kickstarter 						(April 	3)
+
+//Launch Virus							(Feb 	25)
+//Launch (Like a Kitty) Site				(Feb	24)
+//Launch GlobHammer						(Feb    10) 
+//-------------------
+//create the api for toons
+//format the soul file
+//import soul( through streaming or loading  )
+//load all assets related to the soul
+//place soul on the stage
+//trigger animation
+
 
 var Cosmos = function ( $name, $create, $physics ) { 
 	
